78ceb3ac529c6f679c24d77dc48519ad93e6f99a
Sharif <developersharif@yahoo.com>
2025-04-11 16:51:35 +0600

    Refactor Label class to improve structure and enhance option handling

--- src/Widget/Label.php
+++ src/Widget/Label.php
@@ -8,18 +8,53 @@ namespace PhpGui\Widget;
  *
  * @package PhpGui\Widget
  */
-class Label extends AbstractWidget {
-    public function __construct(string $parentId, array $options = []) {
-        parent::__construct($parentId, $options); 
+class Label extends AbstractWidget
+{
+    public function __construct(string $parentId, array $options = [])
+    {
+        parent::__construct($parentId, $options);
         $this->create();
     }
 
-    protected function create(): void {
+    protected function create(): void
+    {
         $text = $this->options['text'] ?? '';
-        $this->tcl->evalTcl("label .{$this->parentId}.{$this->id} -text \"{$text}\""); // Correct Tcl path
+        $extra = $this->getOptionString();
+        $this->tcl->evalTcl("label .{$this->parentId}.{$this->id} -text \"{$text}\" {$extra}");
     }
 
-    public function setText(string $text): void {
-        $this->tcl->evalTcl(".{$this->parentId}.{$this->id} configure -text \"{$text}\""); // Correct Tcl path
+    protected function getOptionString(): string
+    {
+        $opts = "";
+        foreach ($this->options as $key => $value) {
+            if ($key === 'text') continue;
+            $opts .= " -$key \"$value\"";
+        }
+        return $opts;
+    }
+
+    public function setText(string $text): void
+    {
+        $this->tcl->evalTcl(".{$this->parentId}.{$this->id} configure -text \"{$text}\"");
+    }
+
+    public function setFont(string $font): void
+    {
+        $this->tcl->evalTcl(".{$this->parentId}.{$this->id} configure -font \"{$font}\"");
+    }
+
+    public function setForeground(string $color): void
+    {
+        $this->tcl->evalTcl(".{$this->parentId}.{$this->id} configure -fg \"{$color}\"");
+    }
+
+    public function setBackground(string $color): void
+    {
+        $this->tcl->evalTcl(".{$this->parentId}.{$this->id} configure -bg \"{$color}\"");
+    }
+
+    public function setState(string $state): void
+    {
+        $this->tcl->evalTcl(".{$this->parentId}.{$this->id} configure -state \"{$state}\"");
     }
 }
