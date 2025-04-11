0a308b29e6b26880edf7046be6835f27942205e5
Sharif <developersharif@yahoo.com>
2025-04-11 16:51:11 +0600

    Refactor Checkbutton class to improve variable management and callback registration

--- src/Widget/Checkbutton.php
+++ src/Widget/Checkbutton.php
@@ -1,19 +1,61 @@
 <?php
+
 namespace PhpGui\Widget;
 
-class Checkbutton extends AbstractWidget {
-    protected function create(): void {
+use PhpGui\ProcessTCL;
+
+class Checkbutton extends AbstractWidget
+{
+    private $callback;
+    private $variable;
+
+    public function __construct(string $parentId, array $options = [])
+    {
+        parent::__construct($parentId, $options);
+        $this->callback = $options['command'] ?? null;
+        $this->variable = "cb_var_" . $this->id;
+        $this->create();
+    }
+
+    protected function create(): void
+    {
         $text = $this->options['text'] ?? 'Checkbutton';
-        $this->tcl->evalTcl("checkbutton .{$this->parentId}.{$this->id} -text \"{$text}\"");
+        $extra = $this->getOptionString();
+
+        // Initialize variable and register callback if provided
+        $this->tcl->evalTcl("set {$this->variable} 0");
+
+        if ($this->callback) {
+            ProcessTCL::getInstance()->registerCallback($this->id, $this->callback);
+            $this->tcl->evalTcl("checkbutton .{$this->parentId}.{$this->id} -text \"{$text}\" -variable {$this->variable} -command {php::executeCallback {$this->id}} {$extra}");
+        } else {
+            $this->tcl->evalTcl("checkbutton .{$this->parentId}.{$this->id} -text \"{$text}\" -variable {$this->variable} {$extra}");
+        }
     }
-    
-    public function setChecked(bool $state): void {
+
+    public function setChecked(bool $state): void
+    {
         $value = $state ? 1 : 0;
-        $this->tcl->evalTcl("set {$this->id} $value");
+        $this->tcl->evalTcl("set {$this->variable} $value");
     }
-    
-    public function isChecked(): bool {
-        $this->tcl->evalTcl("set _val [set {$this->id}]");
-        return (bool)$this->tcl->getResult();
+
+    public function isChecked(): bool
+    {
+        return (bool)$this->tcl->evalTcl("set {$this->variable}");
+    }
+
+    public function toggle(): void
+    {
+        $this->setChecked(!$this->isChecked());
+    }
+
+    protected function getOptionString(): string
+    {
+        $opts = "";
+        foreach ($this->options as $key => $value) {
+            if (in_array($key, ['text', 'command'])) continue;
+            $opts .= " -$key \"$value\"";
+        }
+        return $opts;
     }
 }
