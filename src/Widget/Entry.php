cc6d7d0e9ba99a52570a49d79cc207d3f631c861
Sharif <developersharif@yahoo.com>
2026-03-26 19:45:45 +0600

    added construct, removed package require ttk , linked textvariable

--- src/Widget/Entry.php
+++ src/Widget/Entry.php
@@ -8,17 +8,22 @@ namespace PhpGui\Widget;
  * @package PhpGui\Widget
  */
 class Entry extends AbstractWidget {
+    public function __construct(string $parentId, array $options = []) {
+        parent::__construct($parentId, $options);
+        $this->create();
+    }
+
     protected function create(): void {
         $defaultText = $this->options['text'] ?? '';
-        $this->tcl->evalTcl("entry .{$this->parentId}.{$this->id}");
+        // Use -textvariable so getValue/setValue reflect the actual widget content.
+        $this->tcl->evalTcl("entry .{$this->parentId}.{$this->id} -textvariable {$this->id}");
         $this->tcl->evalTcl("set {$this->id} \"$defaultText\"");
     }
-    
+
     public function getValue(): string {
-        $this->tcl->evalTcl("set _val [set {$this->id}]");
-        return $this->tcl->getResult();
+        return $this->tcl->getVar($this->id);
     }
-    
+
     public function setValue(string $value): void {
         $this->tcl->evalTcl("set {$this->id} \"$value\"");
     }
