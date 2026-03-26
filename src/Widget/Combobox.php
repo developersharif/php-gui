65788dd366cd44871eb1f22c27d965befc959c67
Sharif <developersharif@yahoo.com>
2026-03-26 19:45:12 +0600

    added contruct, removed package requrie ttk linked textvariable

--- src/Widget/Combobox.php
+++ src/Widget/Combobox.php
@@ -8,17 +8,24 @@ namespace PhpGui\Widget;
  * @package PhpGui\Widget
  */
 class Combobox extends AbstractWidget {
+    public function __construct(string $parentId, array $options = []) {
+        parent::__construct($parentId, $options);
+        $this->create();
+    }
+
     protected function create(): void {
-        $this->tcl->evalTcl("package require ttk");
+        // ttk is built into Tk 8.5+; no separate package require needed after "package require Tk".
         $values = $this->options['values'] ?? '';
-        $this->tcl->evalTcl("ttk::combobox .{$this->parentId}.{$this->id} -values {{$values}}");
+        // Use -textvariable so getValue/setValue reflect the actual widget content.
+        $this->tcl->evalTcl(
+            "ttk::combobox .{$this->parentId}.{$this->id} -textvariable {$this->id} -values {{$values}}"
+        );
     }
-    
+
     public function getValue(): string {
-        $this->tcl->evalTcl("set _val [set {$this->id}]");
-        return $this->tcl->getResult();
+        return $this->tcl->getVar($this->id);
     }
-    
+
     public function setValue(string $value): void {
         $this->tcl->evalTcl("set {$this->id} \"$value\"");
     }
